f90766cee1baa3b21404333c332359629904ba93
ChimeSummit <ringberg.jesper@gmail.com>
2025-04-09 13:51:31 +0200

    Added build script with config to fail and not warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default defineConfig([
-  {
+    ...tseslint.configs.recommendedTypeChecked,
+    {
     files: ["**/*.ts"],
     languageOptions: {
       parser: tseslint.parser,
@@ -10,16 +12,29 @@ export default defineConfig([
         project: "./tsconfig.json",
       },
     },
+        plugins: { "unused-imports": unusedImports },
     rules: {
+      "@typescript-eslint/typedef": ["error", {
+        "arrayDestructuring": true,
+        "arrowParameter": true,
+        "memberVariableDeclaration": true,
+        "objectDestructuring": true,
+        "parameter": true,
+        "propertyDeclaration": true,
+        "variableDeclaration": true,
+        "variableDeclarationIgnoreFunction": false
+      }],
       "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/explicit-function-return-type": "error",
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "no-console": "warn",
       "no-var": "error",
       "prefer-const": "error",
-      "no-unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+            "warn",
+            { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" }
+        ]
     },
   },
-
-  tseslint.configs.recommended,
 ]);
--- src/failingCode.ts
+++ src/failingCode.ts
@@ -8,6 +8,7 @@ foo = 42;  // No error, but using `any` is discouraged.
 function bar(baz: any) {  // No return type declared, should trigger `explicit-function-return-type`
     return "This is a string";
 }
+bar()
 
 // 2. Unused variable, should trigger `@typescript-eslint/no-unused-vars`
 let unusedVar = "I'm not used anywhere";
@@ -17,3 +18,6 @@ let message = "Hello, world!";  // This should be `const` but we are using `let`
 
 // 4. Console log, should trigger `no-console`
 console.log("This is a console log!");  // Using console.log triggers the `no-console` rule.
+
+
+const wtf = "adfd";
